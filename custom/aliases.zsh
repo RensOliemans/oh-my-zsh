9a78e9fe79371ca8e0d3ddc13942c8c3e6c57bd9
Rens Oliemans <rens.oliemans@gmail.com>
2018-09-01 14:38:19 +0200

    :newspaper: Add own aliases

---
+++ custom/aliases.zsh
@@ -0,0 +1,71 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+
+alias py='python3'
+alias randvenv='source ~/venvs/random/bin/activate'
+
+alias spw='py ~/Projects/randomshit/spreekwoorden/spreekwoorden.py'
+alias spreekwoord='spw -a 1'
+
+alias upgrade='sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y'
+
+
+alias balias='vim ~/.oh-my-zsh/custom/aliases.zsh && source ~/.zshrc'
+alias sourcez='source ~/.zshrc'
+
+alias sl='ls'
+
+# VIRTUALENVS
+alias random='source ~/venvs/random/bin/activate'
+alias ml='source ~/venvs/ML/bin/activate'
+alias tmc='source ~/venvs/themecourse/bin/activate'
+
+export DDA='residentiedirtya.student.utwente.nl'
+alias sshdda="ssh pi@$DDA"
+
+alias swaprm='rm ~/.local/share/nvim/swap/*.swp'
+
+# All gitmoji commands
+# Go to gitmoji website
+alias gitmoji='xdg-open https://github.com/slashsBin/styleguide-git-commit-message'
+alias gmo='gitmoji'
+alias gmoj='cat ~/.oh-my-zsh/custom/aliases.zsh | grep "gcmsg"'
+
+# Gitmoji specific shortcuts
+alias gart='gcmsg ":art:'                      # Improving format/structure of code
+alias gnewf='gcmsg ":newspaper:'               # Creating a new file
+alias gtypo='gcmsg ":pencil2:'                 # Performing minor changes/fixes
+alias gspeed='gcmsg ":racehorse:'              # Improving performance
+alias gdocs='gcmsg ":books:'                   # Writing docs
+alias gbug='gcmsg ":bug:'                      # Reporting a bug
+alias gfix='gcmsg ":ambulance:'                # Fixing a bug
+alias gfire='gcmsg ":fire:'                    # Removing code or files
+alias gmove='gcmsg ":tractor:'                 # Changing file structure
+alias grefactor='gcmsg ":hammer:'              # Refactoring code
+alias gtest='gcmsg ":umbrella:'                # Adding tests
+alias gpretty='gcmsg ":lipstick:'              # Improving UI/cosmetics
+alias gconstruction='gcmsg ":construction:'    # Work in progress
+alias gversion='gcmsg ":gem:'                  # New release
+alias gegg='gcmsg ":egg:'                      # New release with Python egg
+alias gtags='gcmsg ":bookmark:'                # Version tags
+alias glogs='gcmsg ":speaker:'                 # Adding logging
+alias grmlogs='gcmsg ":mute:'                  # Reducing logging
+alias gnew='gcmsg ":sparkles:'                 # New features
+alias ginit='gcmsg ":tada:'                    # Initial commit
+alias gconfig='gcmsg ":wrench:'                # Changing configuration files
+alias gugly='gcmsg ":hankey:'                  # Adding code which is bad and has to be changed
+alias grevert='gcmsg ":rewind:'                # Rewinding a commit
+alias gbreaking='gcmsg ":boom:'                # Adding breaking changes
+alias gdrunk='gcmsg ":beers:'                  # Committing code while drunk
+alias gdata='gcmsg ":card_file_box:'           # Changing data(base) files
+alias ggignore='gcmsg ":see_no_evil:'          # Changing .gitignore file
+alias glicense='gcmsg ":page_facing_up:'       # Changing license files
+alias gmerge='gcmsg ":handshake:'              # Merging code
