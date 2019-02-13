b475945b0af7d1affeef9ea52c069fb771fe9e10
Rens Oliemans <rens.oliemans@gmail.com>
2019-02-13 13:51:49 +0100

    :art: Zsh glob

--- custom/main.zsh
+++ custom/main.zsh
@@ -1,6 +1,3 @@
-source /home/rens/.oh-my-zsh/custom/aliases/aliases.zsh
-source /home/rens/.oh-my-zsh/custom/aliases/general.zsh
-source /home/rens/.oh-my-zsh/custom/aliases/git.zsh
-source /home/rens/.oh-my-zsh/custom/aliases/random.zsh
-source /home/rens/.oh-my-zsh/custom/aliases/shortcuts.zsh
-source /home/rens/.oh-my-zsh/custom/aliases/ssh.zsh
+for zsh_file in /home/rens/.oh-my-zsh/custom/aliases/*.zsh; do
+  source $zsh_file
+done
