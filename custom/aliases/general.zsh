e05a787fcba223bd4142fb95ebfe68c8a17306a1
Rens Oliemans <rens.oliemans@gmail.com>
2020-02-18 22:29:43 +0100

    Add start latex command

--- custom/aliases/general.zsh
+++ custom/aliases/general.zsh
@@ -2,3 +2,5 @@ alias py='python3.7'
 
 # Call ipython from within the virtualenv
 alias ipy="python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
+
+alias start-latex='git clone --depth=1 --branch=master git@gitlab.com:RensOliemans/latex-template && rm -rf latex-template/.git'
