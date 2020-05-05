1bfb68c27222746c736aea33aa4c8dca69b474e5
Rens Oliemans <rens.oliemans@gmail.com>
2020-05-05 13:30:32 +0200

    :sparkles: Update start-latex

--- custom/aliases/general.zsh
+++ custom/aliases/general.zsh
@@ -3,4 +3,4 @@ alias py='python3.7'
 # Call ipython from within the virtualenv
 alias ipy="python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
 
-alias start-latex='git clone --depth=1 --branch=master git@gitlab.com:RensOliemans/latex-template && rm -rf latex-template/.git'
+alias start-latex='git clone --depth=1 --branch=master git@gitlab.com:RensOliemans/latex-template && rm -rf latex-template/.git && rm -rf latex-template/LICENSE'
