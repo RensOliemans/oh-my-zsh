3693b6b6396401c372982a1e6cedd4b349a2e1e1
Rens Oliemans <rens.oliemans@gmail.com>
2020-05-07 11:29:06 +0200

    Add vimex ssh

--- custom/aliases/ssh.zsh
+++ custom/aliases/ssh.zsh
@@ -1,7 +1,14 @@
 export DDA='residentiedirtya.student.utwente.nl'
 export HOMEPI='renspi.student.utwente.nl'
 
+export USERZXCS='u71808p69224'
+export HOSTZXCS='web0098.zxcs.nl'
+export PORTZXCS='7685'
+
 alias sshdda="ssh pi@$DDA"
 alias pingdda="ping $DDA"
+
 alias sshpi="ssh pi@$HOMEPI"
 alias pingpi="ping $HOMEPI"
+
+alias sshvimexx="ssh $USERZXCS@$HOSTZXCS -p $PORTZXCS"
